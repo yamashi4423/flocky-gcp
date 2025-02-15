ec37e1906b00df613eb4760510b52962eb8362cb
yamashi4423 <yamashi4423@gmail.com>
2025-02-15 22:00:26 +0900

    langchainへの移行完了

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import openai
 from consts.prompts import SYSTEM_PROMPT
 
 
-def response_text_from_llm(user_message: str, client: openai.OpenAI, model: str) -> str:
-    response = client.chat.completions.create(
-        model=model,
-        messages=[{"role": "system", "content": SYSTEM_PROMPT}, {"role": "user", "content": user_message}]
-    )
-    reply_text = response.choices[0].message.content.strip()
-    return reply_text
+# def response_text_from_llm(user_message: str, client: openai.OpenAI, model: str) -> str:
+#     response = client.chat.completions.create(
+#         model=model,
+#         messages=[{"role": "system", "content": SYSTEM_PROMPT}, {"role": "user", "content": user_message}]
+#     )
+#     reply_text = response.choices[0].message.content.strip()
+#     return reply_text
